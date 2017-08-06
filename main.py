7dcd2c897949e7a48d881180e94247afd6ff5933
Taylor McCreary <taylor@mccreary.cc>
2017-08-05 20:03:13 -0700

    creates gender-based model

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 # read in training data
 train_df = pd.read_csv('train.csv')
-print(list(train_df.columns.values))
 
 """This is the part where I need to build a model"""
 
 # read in test data
 test_df = pd.read_csv('test.csv')
-print(list(test_df.columns.values))
+
+# gender_df will be my model used to predict survival based solely on gender
+gender_df = train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=True).mean()
+print(gender_df.loc['female','Survived'])
+print(gender_df.loc['male','Survived'])
+
 
 """This is the part where I need to use my model to predict survival on the test data"""
 
@@ -18,6 +22,10 @@ output_df = pd.DataFrame(index=test_df['PassengerId'])
 
 # currently, just predicting that everyone dies
 output_df['Survived'] = 0
+
+## NEXT: iterate & apply model
+for index, row in df.iterrows() :
+    print(row['A'], row['B'])
 
 # exporting the DataFrame of ids and predictions into a .csv
 output_df.to_csv('submission.csv')
