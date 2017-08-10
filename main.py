816ccd89dee80bd9b44494453df41cb2c3d89095
Taylor McCreary <taylor@mccreary.cc>
2017-08-10 12:52:48 -0700

    using pandas apply to iterate through df

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 """This is the main script which will be executed"""
 import pandas as pd
 
-def get_survival(passenger, gender_probs):
-    """ Takes as input a row of a data frame corresponding to a passenger,
-    and a data frame with the probabilites of survival by gender"""
-    p_gender = passenger['Sex']
-    return int(round(gender_probs.loc[p_gender,'Survived']))
 
 # read in training data
 train_df = pd.read_csv('train.csv')
@@ -16,7 +11,15 @@ train_df = pd.read_csv('train.csv')
 # get_survival will be the function that uses a gender based model to predict survival
 gender_df = train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=True).mean()
 
-"""This is the part where I need to use my model to predict survival on the test data"""
+def get_survival(passenger):
+    """ Takes as input a row of a data frame corresponding to a passenger,
+    and a data frame with the probabilites of survival by gender"""
+    p_gender = passenger['Sex']
+    res = int(round(gender_df.loc[p_gender, 'Survived']))
+    return res
+
+
+#"""This is the part where I need to use my model to predict survival on the test data"""
 
 # read in test data
 test_df = pd.read_csv('test.csv')
@@ -27,6 +30,7 @@ output_df = pd.DataFrame(index=test_df['PassengerId'])
 output_df['Survived'] = 0
 
 # iterate through the test data, using the model to compute the Survival
+"""
 for index, row in test_df.iterrows() :
     # get the output row corresponding to this row in test_df
     # that row, the 'Survived' column should be set to be a function of the test_df row
@@ -37,6 +41,8 @@ for index, row in test_df.iterrows() :
     # testval has to match the type that the 'Survived' column originally was.
     # In this case, they are both ints.
     output_df.set_value(ind, 'Survived', testval)
+"""
+test_df['Survived'] = test_df.apply(get_survival, axis=1)
 
 # exporting the DataFrame of ids and predictions into a .csv
-output_df.to_csv('submission.csv')
+test_df[['PassengerId','Survived']].to_csv('submission.csv', index=False)
