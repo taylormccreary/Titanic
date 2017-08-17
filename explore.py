d71bb8e7c02b3558a1c5d57a46c6a20438ef41bc
Taylor McCreary <taylor@mccreary.cc>
2017-08-17 12:44:38 -0700

    Uses matplotlib to make a simple histogram

--- explore.py
+++ explore.py
@@ -1,16 +1,29 @@
 """This will be a script for preliminary exploration of the data"""
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('train.csv')
+# x = np.linspace(0, 1, 201)
+# y = np.sin((2*np.pi*x)**2)
 
+# plt.plot(x, y, 'red')
+# plt.show()
+
+
+
+df = pd.read_csv('train.csv')
+x = df['Age']
+y = df['Survived']
+#plt.scatter(x, y)
+plt.hist(x.dropna(), bins=15)
+plt.show()
 
 #for index, row in df.iterrows() :
 #    print(row['Sex'], row['Survived'])
 
-print(df.columns.values)
-print(df.describe())
+#print(df.columns.values)
+#print(df.describe())
 
 #df.plot()
 #plt.show()
