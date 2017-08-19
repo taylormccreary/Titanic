3e21179cff1294d1d8c370175cfc2f0be1128f28
Taylor McCreary <taylor@mccreary.cc>
2017-08-19 16:12:31 -0700

    using a decision tree

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 """This is the main script which will be executed"""
 import pandas as pd
+from sklearn import tree
 
 # read in training data
 train_df = pd.read_csv('train.csv')
 
-# gender_df will be my model used to predict survival based solely on gender
-# get_survival will be the function that uses a gender based model to predict survival
-gender_df = train_df[['Sex', 'Survived']].groupby(['Sex'], as_index=True).mean()
+# Now let's make a decision tree with it!
+clf = tree.DecisionTreeClassifier()
+
+# in order to do this, we need to clean up the data first
+
+# make all the male and female values for Sex 1s and 0s
+train_df['Sex'] = train_df['Sex'].map({'female': 1, 'male': 0})
+train_df['Sex'].fillna(-1, inplace=True)
+# Make all the NaN Pclass values -1
+train_df['Pclass'].fillna(-1, inplace=True)
+# Make all the NaN Age values -1
+train_df['Age'].fillna(-1, inplace=True)
+
+clf = clf.fit(train_df.loc[:, ['Pclass', 'Sex', 'Age']], train_df.loc[:, 'Survived'])
 
 def get_survival(passenger):
     """ Takes as input a row of a data frame corresponding to a passenger,
-    and uses a data frame with the probabilites of survival by gender"""
-    p_gender = passenger['Sex']
-    res = int(round(gender_df.loc[p_gender, 'Survived']))
+    and uses a decision tree classifier to assign a value to 'Survived' """
+    passenger = passenger['Pclass', 'Sex', 'Age']
+    res = clf.predict(passenger)
     return res
 
 # read in test data
 test_df = pd.read_csv('test.csv')
+# clean test data as well
+# make all the male and female values for Sex 1s and 0s
+test_df['Sex'] = train_df['Sex'].map({'female': 1, 'male': 0})
+test_df['Sex'].fillna(-1, inplace=True)
+# Make all the NaN Pclass values -1
+test_df['Pclass'].fillna(-1, inplace=True)
+# Make all the NaN Age values -1
+test_df['Age'].fillna(-1, inplace=True)
 
 # iterate through the test data, using the model to compute the Survival
 # the Survived column is being added to the test data DataFrame
